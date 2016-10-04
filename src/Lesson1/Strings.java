1907edb575dd779ce8ac1c408d975b3a9bf36937
Demnart <insidia93@gmail.com>
2016-10-04 14:36:38 +0300

    Initial commit

---
+++ src/Lesson1/Strings.java
@@ -0,0 +1,12 @@
+package Lesson1;
+
+/**
+ * Created by Артем on 04.10.2016.
+ */
+public class Strings {
+    public static void main(String[] args) {
+        String s;
+        s = "helloworld";
+        System.out.println(s);
+    }
+}
