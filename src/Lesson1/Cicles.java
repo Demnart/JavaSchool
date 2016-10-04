d9e1ea0514e17457533f816e0ed6d70bd8422f90
Demnart <insidia93@gmail.com>
2016-10-04 14:40:22 +0300

    Первые домашние задания

---
+++ src/Lesson1/Cicles.java
@@ -0,0 +1,13 @@
+package Lesson1;
+
+/**
+ * Created by Артем on 04.10.2016.
+ */
+public class Cicles {
+    public static void main(String[] args) {
+        for (int i = 0; i < 40; i++) {
+            System.out.println("Привет разбойник номер " + i);
+            System.out.println("Привет колобок");
+        }
+    }
+}
---
+++ src/Lesson1/New.java
@@ -0,0 +1,17 @@
+package Lesson1;
+
+/**
+ * Created by Артем on 04.10.2016.
+ */
+public class New {
+    public static void main(String[] args) {
+        int i, N = 20;
+
+        for (i = 0; i < N; i++){
+
+            System.out.println("*");
+
+        }
+
+    }
+}
