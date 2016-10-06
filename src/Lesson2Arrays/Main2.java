57277121542b24aa5b97f0e3bd46f9868e089ef9
Артем <insidia93@gmail.com>
2016-10-06 13:16:09 +0300

    работа с массивами

---
+++ src/Lesson2Arrays/Main2.java
@@ -0,0 +1,13 @@
+package Lesson2Arrays;
+
+/**
+ * Created by Алина on 04.10.2016.
+ */
+public class Main2 {
+    public static void main(String[] args) {
+        char[][] cells = new char[3][];
+
+
+    }
+
+}
