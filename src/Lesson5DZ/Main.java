5daaf23726af8eb6aea1cebf4ceffda15755e414
Demnart <insidia93@gmail.com>
2016-10-13 16:01:22 +0300

    Домашнее задание с наследованием и полиморфизмом

---
+++ src/Lesson5DZ/Main.java
@@ -0,0 +1,14 @@
+package Lesson5DZ;
+
+/**
+ * Created by Артем on 13.10.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Figure figure = new Figure();
+        Circle circle = new Circle();
+        Figure figure1 = new Circle();
+//        Circle circle2 =  new Figure(); Ошибка
+
+    }
+}
