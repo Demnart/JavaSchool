92793cd2bb2d92f7e236979e02d29b60703c520d
Demnart <insidia93@gmail.com>
2016-10-13 16:00:48 +0300

    Домашнее задание с машиной и колесами

---
+++ src/Lesson5CarDZ/Car.java
@@ -0,0 +1,11 @@
+package Lesson5CarDZ;
+
+/**
+ * Created by Артем on 13.10.2016.
+ */
+public class Car {
+    Wheel[] wheels = new Wheel[4];
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Lesson5CarDZ/Wheel.java
@@ -0,0 +1,7 @@
+package Lesson5CarDZ;
+
+/**
+ * Created by Артем on 13.10.2016.
+ */
+public class Wheel {
+}
